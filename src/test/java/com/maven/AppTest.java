ab99806cf24c0841360ce6da393e4500de44e447
Kesavan Ramasamy <nikil.kaarthi@gmail.com>
2017-06-30 12:52:13 -0400

    adding

---
+++ src/test/java/com/maven/AppTest.java
@@ -0,0 +1,21 @@
+package com.maven;
+
+import org.testng.TestNG;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestNG
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( );
+    }
+
+}
---
+++ src/test/java/com/maven/testngmaven.java
@@ -0,0 +1,16 @@
+package com.maven;
+
+import org.testng.annotations.Test;
+
+public class testngmaven {
+	
+	@Test
+	  public void login() {
+		  System.out.println("LOGIN");
+	  }
+	  @Test
+	  public void logout() {
+		  System.out.println("LOGOUT");
+
+}
+}
